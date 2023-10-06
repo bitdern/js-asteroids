5b46bd52034a698285019ea0841d7a86bd73e439
bitdern <acnewell09@gmail.com>
2023-10-06 17:59:07 -0500

    one more

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const canvas = document.querySelector("canvas");
 
+// where variable c represents the context of the HTML canvas
+// context being the object that represents the state of the canvas
+const c = canvas.getContext("2d");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+// setting background for the gamespace
+c.fillStyle(black);
+c.fillRect(0, 0, canvas.width, canvas.height);
