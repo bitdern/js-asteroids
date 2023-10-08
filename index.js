b375672baa6d7ff0608643c003b4df4fca84052c
bitdern <acnewell09@gmail.com>
2023-10-08 11:23:18 -0500

    adding listeners to move player

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 // setting background for the gamespace
-c.fillStyle(black);
+c.fillStyle = "black";
 c.fillRect(0, 0, canvas.width, canvas.height);
 
-// creating the user character (lil triangle guy)
+// initial values for triangle spaceship
 class Player {
   constructor({ position, velocity }) {
     this.position = position; // {x, y}
@@ -19,6 +19,10 @@ class Player {
   }
 
   draw() {
+    c.arc(this.position.x, this.position.y, 5, 0, Math.PI * 2, false);
+    c.fillStyle = "red";
+    c.fill();
+
     c.moveTo(this.position.x + 30, this.position.y);
     c.lineTo(this.position.x - 10, this.position.y - 10);
     c.lineTo(this.position.x - 10, this.position.y + 10);
@@ -29,6 +33,7 @@ class Player {
   }
 }
 
+// setting initial player position
 const player = new Player({
   position: { x: canvas.width / 2, y: canvas.height / 2 },
   velocity: { x: 0, y: 0 },
@@ -36,4 +41,35 @@ const player = new Player({
 
 player.draw();
 
-console.log(player);
+const keys = {
+  w: {
+    pressed: false,
+  },
+};
+
+function animate() {
+  window.requestAnimationFrame(animate);
+
+  player.draw;
+
+  if (keys.w.pressed) player.velocity.x = 1;
+}
+
+animate();
+
+// functionality that listens for keyboard events
+// the spaceship will move based upon these events
+window.addEventListener("keydown", () => {
+  switch (e.code) {
+    case "KeyW":
+      keys.w.pressed = true;
+      break;
+    case "KeyA":
+      // logic for a key press
+      break;
+    case "KeyD":
+      // logic for d key press
+      break;
+  }
+  console.log(e);
+});
